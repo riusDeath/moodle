05ca5787a0361f8e351e8c7dd281b9608340f633
riusDeath <vuthithuy10197@gmail.com>
2019-03-20 10:00:23 +0700

    push code moodle

--- mod/quiz/listquiz.php
+++ mod/quiz/listquiz.php
@@ -28,6 +28,8 @@ require_once($CFG->dirroot.'/mod/quiz/locallib.php');
 require_once($CFG->libdir . '/completionlib.php');
 require_once($CFG->dirroot . '/course/format/lib.php');
 
+defined('MOODLE_INTERNAL') || die();
+
 // Set up the page.
 $PAGE->set_url($CFG->wwwroot.'/mod/quiz/listquiz.php');
 $PAGE->set_context(get_system_context());
@@ -39,48 +41,15 @@ echo $OUTPUT->header();
 // mod_quiz_get_quizzes_by_courses
 
 // $PAGE->set_context(get_system_context());
+$result = $DB->get_records("quiz", array()); // get all records in mdl_user table
 
-// $coursecontext = context_course::instance(3);
-// $categories = $DB->get_records_menu('question_categories', array('contextid'=>$coursecontext->id), 'name', 'id, name');
-// $quiz = $DB->get_records_menu('quiz', array('course' => 3), 'id');
-// foreach ($categories as $key => $value) {
-// 	# code...
-// 	echo $value;
-// }
+foreach($result as $record){
 
-$params = array();
-list($udeletedsql , $param) = $DB->get_in_or_equal(0, SQL_PARAMS_NAMED);
-$params += $param;
-list($enrolsql, $param) = $DB->get_in_or_equal('manual', SQL_PARAMS_NAMED);
-$params += $param;
-list($coursesql, $param) = $DB->get_in_or_equal(8, SQL_PARAMS_NAMED);
-$params += $param;
-list($usersql, $param) = $DB->get_in_or_equal($selID, SQL_PARAMS_NAMED);
-$params += $param;
-$activities = null;
-$quiz = $DB->get_record_sql('SELECT COUNT(*) FROM {quiz} ', $param, MUST_EXIST);
-echo $quiz;
+    echo $record->name;
 
-// // $quiz = $DB->get_records_menu('quiz');
-// // $quiz = context_quiz::instance();
-// $quiz = $DB->get_records_sql_menu("SELECT * FROM quiz");
-// foreach ($quiz as $key => $value) {
-// echo $quiz[$key];
-// 	# code...
-// }
+}
 
-// foreach ($quiz as $key => $value) {
-// 	echo $OUTPUT->box_start();
-// 	$btncourse = new single_button(new moodle_url('mod/quiz/view.php'), "Course", 'get');
-// 	echo $OUTPUT->render_single_button(new single_button(new moodle_url('view.php',array('id' => $key)), "quiz", 'get'));
-// 	echo $OUTPUT->box_end();
-// 	// echo $key;
-// 	// echo "<a href='"+new moodle_url('mod/quiz/view.php',array('id' => $key))+"''>Link quiz</a>";
-// }
+
 echo $OUTPUT->footer();
 
-
-// foreach ($categories as $key => $value) {
-// 	echo $key;
-// }
 ?>
